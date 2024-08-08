63f2d24cc27e5a5c2f948d82df7d639e2c44540c
max <148343900+Blxstedlol@users.noreply.github.com>
2024-08-08 17:02:28 +0100

    Completed Auto Update Script

--- Auto Update Script/script.py
+++ Auto Update Script/script.py
@@ -1,14 +1,16 @@
 import requests
 def check():
-    url = 'https://raw.githubusercontent.com/Blxstedlol/Python-Scripts/main/Auto%20Update%20Script/version.txt'
-    response = requests.get(url=url)
-    if response == 200:
-        latest_version = response.text
-        with open('version.txt', 'r') as file:
-            current_version = file.read()
-            file.close()
+    version_url = 'https://raw.githubusercontent.com/Blxstedlol/Python-Scripts/main/Auto%20Update%20Script/version.txt'
+    response = requests.get(url=version_url)
+    latest_version = response.text
+    print(latest_version)
+    with open('version.txt', 'r') as file:
+        current_version = file.read().strip()
+        
+        print(current_version)
+    if current_version < latest_version:
         with open('version.txt', 'w') as file:
-            if latest_version > current_version:
-                file.write(latest_version)
-                print('Latest Version has been installed!')
+            file.write(f'{latest_version}')
+            print('Updated Version.txt')
+    
 check()
