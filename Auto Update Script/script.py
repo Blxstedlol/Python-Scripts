4dae2c8b8c9ae1da7f3182df6544189fa0d3c14b
max <148343900+Blxstedlol@users.noreply.github.com>
2024-08-08 16:48:14 +0100

    Got Auto Update Script working

--- Auto Update Script/script.py
+++ Auto Update Script/script.py
@@ -1,4 +1,14 @@
 import requests
 def check():
-    url = ''
-    requests.get()
+    url = 'https://raw.githubusercontent.com/Blxstedlol/Python-Scripts/main/Auto%20Update%20Script/version.txt'
+    response = requests.get(url=url)
+    if response == 200:
+        latest_version = response.text
+        with open('version.txt', 'r') as file:
+            current_version = file.read()
+            file.close()
+        with open('version.txt', 'w') as file:
+            if latest_version > current_version:
+                file.write(latest_version)
+                print('Latest Version has been installed!')
+check()
